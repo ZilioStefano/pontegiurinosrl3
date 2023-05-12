5d543ae3b56d87ca9295f0718b802031fa1be891
stefanoZS <stefano.trevisan@zilioservice.com>
2023-05-12 22:37:51 +0200

    Add Azure deployment files

--- food/views.py
+++ food/views.py
@@ -1,32 +1,30 @@
 # food/views.py
 from django.shortcuts import render, redirect
 from ftplib import FTP
-import pandas as pd
+# import pandas as pd
 # import matplotlib.pyplot as plt
 # from io import BytesIO
 # import base64
 
-def uploadData():
-
-    # ftp = FTP("192.168.10.211", timeout=120)
-    ftp = FTP("93.33.192.68", timeout=120)
-
-    ftp.login('ftpdaticentzilio', 'Sd2PqAS.We8zBK')
-    ftp.cwd('/dati/ponte_giurino')
-
-    gFile = open('PGDailyPlot.csv', "wb")
-    ftp.retrbinary('RETR PGDailyPlot.csv', gFile.write)
-    gFile.close()
-
-    Data = pd.read_csv("PGDailyPlot.csv")
-    # Data = 0
-    return Data
+# def uploadData():
+#
+#     # ftp = FTP("192.168.10.211", timeout=120)
+#     ftp = FTP("93.33.192.68", timeout=120)
+#
+#     ftp.login('ftpdaticentzilio', 'Sd2PqAS.We8zBK')
+#     ftp.cwd('/dati/ponte_giurino')
+#
+#     gFile = open('PGDailyPlot.csv', "wb")
+#     ftp.retrbinary('RETR PGDailyPlot.csv', gFile.write)
+#     gFile.close()
+#
+#     # Data = pd.read_csv("PGDailyPlot.csv")
+#     # Data = 0
+#     return Data
 
 
 
 def index(request):
-
-
 
     # Data = uploadData()
     #
