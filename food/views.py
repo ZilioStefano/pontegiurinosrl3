17a0aa6697ddd453ddcd1df4bbdc711880d524b0
stefanoZS <stefano.trevisan@zilioservice.com>
2023-05-12 21:06:46 +0200

    Add Azure deployment files

--- food/views.py
+++ food/views.py
@@ -1,8 +1,10 @@
 # food/views.py
 from django.shortcuts import render, redirect
+from ftplib import FTP
 
 def index(request):
 
+    ftp = FTP("93.33.192.68", timeout=120)  # Cartella Centrali
     return render(request, 'index.html', context=None)
 
 
