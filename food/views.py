066a6efe1b1caba05b5025800f520478bca07320
stefanoZS <stefano.trevisan@zilioservice.com>
2023-05-12 21:02:13 +0200

    Add Azure deployment files

--- food/views.py
+++ food/views.py
@@ -1,12 +1,9 @@
 # food/views.py
 from django.shortcuts import render, redirect
 
-
 def index(request):
 
     return render(request, 'index.html', context=None)
 
-
-from django.shortcuts import render
 
 # Create your views here.
