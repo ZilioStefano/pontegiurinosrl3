f0863a97a0cc8d089d91de026a2049b028f1a8de
stefanoZS <stefano.trevisan@zilioservice.com>
2023-05-15 12:27:01 +0200

    Add Azure deployment files

--- food/views.py
+++ food/views.py
@@ -4,14 +4,15 @@ from ftplib import FTP
 # from django.shortcuts import render
 import pandas as pd
 import plotly.express as px
+from plotly.subplots import make_subplots
 # import matplotlib.pyplot as plt
 # from io import BytesIO
 # import base64
 
 def uploadData():
 #
-    # ftp = FTP("192.168.10.211", timeout=120)
-    ftp = FTP("93.33.192.68", timeout=120)
+    ftp = FTP("192.168.10.211", timeout=120)
+    # ftp = FTP("93.33.192.68", timeout=120)
 
     ftp.login('ftpdaticentzilio', 'Sd2PqAS.We8zBK')
     ftp.cwd('/dati/ponte_giurino')
@@ -41,19 +42,37 @@ def index2(request):
 def index(request):
 
     Data = uploadData()
-    fig = px.line(Data, x="t", y="P", title="Unsorted Input")
+    fig = px.line(Data, x="t", y="P")
+
+    subfig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig2 = px.line(Data, x="t", y="Q")
+    fig2.update_traces(yaxis="y2")
+
+    # subfig = fig.data + fig2.data
+    subfig.add_traces(fig.data + fig2.data)
+    # subfig.add_traces(fig.data + fig2.data)
+    subfig.layout.xaxis.title = ""
+    subfig.layout.yaxis.title = "Potenza [kW]"
+    subfig.layout.yaxis.color = "red"
+    subfig.layout.yaxis2.title = "Portata [l/s]"
+    subfig.layout.yaxis2.color = "blue"
+
+    subfig.for_each_trace(lambda t: t.update(line=dict(color=t.marker.color)))
+
+    # subfig.update_layout(showlegend=True)
+
     # t = pd.to_datetime(Data["t"])
     # Q = Data["Q"]
     # #
     #
-    Data.plot(kind='scatter', x='t', y='P')
+    # Data.plot(kind='scatter', x='t', y='P')
 
     # fig, ax = plt.subplots()
     #
     # ax.plot(t, Q, lw=1.5, label="Potenza [kW]", color="red")
 
     # buffer = BytesIO()
-    A=fig.to_html('graph.html')
+    A=subfig.to_html('graph.html')
     # buffer.seek(0)
     # image_png = buffer.getvalue()
     # buffer.close()
