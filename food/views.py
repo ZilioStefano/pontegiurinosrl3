d3ee077700a4eb9f24fb7f39014d9e7ff9207529
stefanoZS <stefano.trevisan@zilioservice.com>
2024-03-06 14:31:56 +0100

    impostato IP pubblico

--- food/views.py
+++ food/views.py
@@ -15,11 +15,11 @@ from num2string_001 import convertNumber
 def uploadData():
 
     # ftp = FTPLogIn()
-    # ftp = FTP("93.33.192.68", timeout=120)
+    ftp = FTP("93.33.192.68", timeout=120)
 
     # try:
     #
-    ftp = FTP("192.168.10.211", timeout=120)
+    # ftp = FTP("192.168.10.211", timeout=120)
     #
     # except:
     #
